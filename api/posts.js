433e0efbf5c71014d5d5609ea2e472e812fe64bc
Joan Sebastian Morales Pérez <josmoralesp0307@outlook.es>
2021-11-03 15:09:16 -0500

    Agregando PATCH en posts

--- api/posts.js
+++ api/posts.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post('/addPost' , postController.addPost);
 router.get('/all' , postController.findAllPost);
 router.get('/:id' , postController.findPostId);
+router.patch('/:id', postController.editAll)
 router.delete('/:id' , postController.remove);
 
 module.exports = router;
--- controllers/posts.js
+++ controllers/posts.js
@@ -38,6 +38,22 @@ const findPostId = (req , res) => {
     } )
 }
 
+const editAll = async(req, res) => {
+    try {
+        const { body } = req;
+        const filter = { _id: req.params.id }
+        const update = body;
+
+        const doc = await Posts.findOneAndUpdate(filter, update, { new: true });
+
+        if (!doc)
+            return res.status(404).json({ message: `_id ${req.params.id} doesn't exists` });
+        res.status(200).json(doc);
+    } catch (error) {
+        return res.status(500).json({ message: error })
+    }
+}
+
 const remove = async(req,res) => {
     try {
         console.log(req.params.id);
@@ -52,4 +68,4 @@ const remove = async(req,res) => {
 
 
 
-module.exports = { addPost , findAllPost , findPostId , remove}
+module.exports = { addPost , findAllPost , findPostId , remove, editAll}
